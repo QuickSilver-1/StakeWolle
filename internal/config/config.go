0a632474388e223f19d4b8ebceac30cce53aba1e
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2024-11-13 20:25:37 +0300

    1

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,9 +3,10 @@ package config
 import "flag"
 
 func init() {
-	httpPort := flag.Int("port", 8080, "port on which the application will run")
-	pgHost := flag.String("host", "89.46.131.181", "host for PostgreSQL database")
-	pgName := flag.String("dbname", "89.46.131.181", "host for PostgreSQL database")
-	pgUser := flag.String("dbuser", "roman", "username for PostgreSQL database")
-	pgPass := flag.String("dbpass", "030905romaN", "password for PostgreSQL database")
+	HttpPort := flag.Int("port", 8080, "port on which the application will run")
+	PgHost := flag.String("host", "89.46.131.181", "host for PostgreSQL database")
+	PgPort := flag.Int("dbport", 5432, "port for PostgreSQL database")
+	PgName := flag.String("dbname", "89.46.131.181", "host for PostgreSQL database")
+	PgUser := flag.String("dbuser", "roman", "username for PostgreSQL database")
+	PgPass := flag.String("dbpass", "030905romaN", "password for PostgreSQL database")
 }
--- pkg/userhandler.go
+++ pkg/userhandler.go
@@ -1,7 +1,18 @@
 package pkg
 
-import "net/http"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/QuickSilver-1/StakeWolle/internal/config"
+	_ "github.com/lib/pq"
+)
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	config.PgHost, config.PgPort, config.PgUser, config.PgPass, сonfig.PgName)
+    db, err := sql.Open("postgres", psqlInfo)
+	fmt.Print(db, err)
 }
